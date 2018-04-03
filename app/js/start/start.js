6f2d03422125975fa17b99ceda9d99cdbf1edea1
Paul <rahveiz@gmail.com>
2018-04-03 23:10:22 +0200

    attention animation de kéké en cours

--- app/js/start/start.js
+++ app/js/start/start.js
@@ -5,3 +5,11 @@ const wp = new WallPaper();
 setInterval(() => {
   wp.refresh();
 }, 50);
+
+$(document).ready(() => {
+  window.setTimeout(() => {
+    $('#t1').addClass('anim1');
+    $('#t2').addClass('anim2');
+    $('a').addClass('anim3');
+  }, 3000);
+});
