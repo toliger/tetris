2cfd062307b7d88248891f9407b02af3ea2d2849
Paul <rahveiz@gmail.com>
2018-05-11 22:30:56 +0200

    update

--- app/js/Game.js
+++ app/js/Game.js
@@ -111,6 +111,8 @@ export default class Game {
   }
 
   restartMulti() {
+    $('#start').toggleClass('active');
+    $('#restartMulti').toggleClass('active');
     this.gameBoard.restartGame();
     this.gameBoard.rules.ingame = true;
     if (this.gameBoard.rules.multiplayer) {
